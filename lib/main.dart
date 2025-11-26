60080d9933d7af0ec9264e596dba4e1227f354d2
zEchth <fauzanfath099@gmail.com>
2025-11-26 19:03:13 +0800

    perbaikan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'features/autentikasi/login_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tugas_kuliyeah/core/providers.dart';
@@ -9,13 +10,26 @@ import 'package:tugas_kuliyeah/features/mata_kuliah/mata_kuliah_list_screen.dart
 
 void main() async {
   // Pastikan binding siap sebelum menjalankan APP
-  
   WidgetsFlutterBinding.ensureInitialized();
+
   final notificationService = NotificationService();
   await notificationService.init();
+
+  // Perbaikan
+  await dotenv.load(fileName: ".env");
+
+  final supabaseUrl = dotenv.env['SUPABASE_URL'];
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'];
+
+  if (supabaseUrl == null || supabaseAnonKey == null) {
+    throw Exception(
+      ".env tidak lengkap! SUPABASE_URL/SUPABASE_ANON_KEY hilang",
+    );
+  }
+
   await Supabase.initialize(
-    url: 'https://zjpswhmhfvapcquscmvd.supabase.co', // Ganti dengan URL proyek Supabase Anda
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqcHN3aG1oZnZhcGNxdXNjbXZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NzE4NjEsImV4cCI6MjA3OTA0Nzg2MX0.kdJgx-4kbzM0HzDXgdyoZrW2g1aXA8ue8NQX0Mdf4d8',
+    url: supabaseUrl,
+    anonKey: supabaseAnonKey,
   );
 
   runApp(
@@ -52,13 +66,14 @@ class AuthGate extends StatelessWidget {
     return StreamBuilder(
       stream: Supabase.instance.client.auth.onAuthStateChange,
       builder: (context, snapshot) {
-        final session = snapshot.data?.session ?? 
+        final session =
+            snapshot.data?.session ??
             Supabase.instance.client.auth.currentSession;
-        
+
         if (session == null) {
           return LoginPage();
-        } 
-          
+        }
+
         return MataKuliahListScreen();
       },
     );
