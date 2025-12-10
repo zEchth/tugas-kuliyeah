9192547a738577c6f8ef2b2a609b75d4e7a583ae
zEchth <fauzanfath099@gmail.com>
2025-12-10 08:44:18 +0800

    Tambah konfigurasi fcm

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -51,6 +52,18 @@ android {
 // tambah ini
 dependencies {
    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
+
+   // Import the Firebase BoM
+   implementation(platform("com.google.firebase:firebase-bom:34.6.0"))
+   implementation("com.google.firebase:firebase-messaging")
+   
+   // TODO: Add the dependencies for Firebase products you want to use
+   // When using the BoM, don't specify versions in Firebase dependencies
+   implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
 
 kotlin {
@@ -60,3 +73,4 @@ kotlin {
 flutter {
     source = "../.."
 }
+
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,11 @@ allprojects {
     }
 }
 
+plugins {
+  // Add the dependency for the Google services Gradle plugin
+  id("com.google.gms.google-services") version "4.4.4" apply false
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
